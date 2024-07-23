e8b20341a912328d24af8d7fbc5009f63cc9e9ff
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-07-22 23:57:54 -0600

    change noteId to id, added comments, added GET request for a single note

--- db/db.js
+++ db/db.js
@@ -1,4 +1,4 @@
-const noteId = () => {
+const id = () => {
   return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
@@ -8,8 +8,8 @@ const notes = [
   {
     title: "Test Title",
     text: "Test text",
-    note_id: noteId(),
+    id: id(),
   },
 ];
 
-module.exports = notes;
+module.exports = { notes, id };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const notes = require("./db/db");
+const { notes, id } = require("./db/db");
 
 const app = express();
 const PORT = 3001;
@@ -9,18 +9,43 @@ const PORT = 3001;
 app.use(express.static("public"));
 // Middleware for the parsing of JSON data
 app.use(express.json());
-// Middleware for parsing of URL encoded date
+// Middleware for parsing of URL encoded data
 app.use(express.urlencoded({ extended: true }));
 
+// Route to serve notes.html file
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+// Route to serve index.html file when root endpoint is accessed
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
-app.get("/api/notes", (req, res) => res.json(notes));
+// GET request for all notes
+app.get("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to get notes`);
+  return res.status(200).json(notes);
+});
+
+// GET request for a single note
+app.get("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send("Note ID not found!");
+  }
+
+  console.info(`${req.method} request received to get a single note`);
+
+  const note = notes.find((note) => note.id == id);
+
+  if (!note) {
+    return res.status(404).json("Note not found!");
+  }
+
+  res.status(200).json(note);
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
