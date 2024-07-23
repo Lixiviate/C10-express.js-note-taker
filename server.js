5ec0543ed2b2f76820d2bf107f9eb8a5dc4b2df5
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-07-22 23:17:33 -0600

    Added app.get to link db.json, added middleware

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const path = require("path");
+const notes = require("./db/db.json");
 
 const app = express();
 const PORT = 3001;
 
+// Middleware to serve static files from "Public" Directory
 app.use(express.static("public"));
+// Middleware for the parsing of JSON data
+app.use(express.json());
+// Middleware for parsing of URL encoded date
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("*", (req, res) =>
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
+
+app.get("/api/notes", (req, res) => res.json(notes));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
