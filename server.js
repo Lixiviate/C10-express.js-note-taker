cb66cb2cb3a19d25b023f41dc47440e467f66a62
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-07-24 22:55:17 -0600

    Updated port info

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { notes, id } = require("./db/db");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to serve static files from "Public" Directory
 app.use(express.static("public"));
